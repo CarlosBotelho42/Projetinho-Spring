1ef3a3a10241263143e914206b686b6ffb54ade2
CarlosBotelho42 <carlospiece55@gmail.com>
2019-11-26 15:00:09 -0300

    Entidade do usuario e recursos

---
+++ src/main/java/com/projetinho/projetinho/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.projetinho.projetinho.resources;
+
+import com.projetinho.projetinho.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //para avisar que implemnta um controlador rest
+@RequestMapping(value = "/users") //nome do recurso (localhost:8080/users)
+
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+         User u = new User(1L,"Carlos", "Carlos.c@gmail.com","69696969","12345");
+         return ResponseEntity.ok().body(u);
+
+    }
+
+}
