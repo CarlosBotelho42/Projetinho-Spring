5226da3d84967aa0c6f7028f44756de166bb706b
CarlosBotelho42 <carlospiece55@gmail.com>
2019-11-26 15:37:42 -0300

    BD H2, test profile, JPA

--- src/main/java/com/projetinho/projetinho/entities/User.java
+++ src/main/java/com/projetinho/projetinho/entities/User.java
@@ -1,9 +1,16 @@
 package com.projetinho.projetinho.entities;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class User implements Serializable { //transforma em bits pra ir pra rede
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String email;
